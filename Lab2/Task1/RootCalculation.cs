f965e471dd3c7ce62c6cdd9c5e255b65ef9737d1
AlexanderKravchenkoS <73443749+AlexanderKravchenkoS@users.noreply.github.com>
2021-03-24 16:50:41 +0300

    Add files via upload

---
+++ Lab2/Task1/RootCalculation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class RootCalculation
+    {
+        public double PowRoot { get; private set; }
+        public double NewtonRoot { get; private set; }
+        public double DifferenceBetweenRoots { get; private set; }
+
+        public RootCalculation(int number, int degree)
+        {
+            NewtonRoot = NewtonRootCalculation(number, degree);
+            PowRoot = Math.Pow(number, (double)1 / degree);
+            DifferenceBetweenRoots = FindDifferenceBetweenTwoWaysOfCalculations(NewtonRoot, PowRoot);
+        }
+
+        public double NewtonRootCalculation(int number, int degree)
+        {
+            int nearestNumberWithRoot = FindNearestIntegerRoot(number, degree);
+            double dividedNumber = (double)number / Math.Exp(Math.Log(nearestNumberWithRoot) * (degree - 1));
+            double result = ((degree - 1) * nearestNumberWithRoot + dividedNumber) / (double)degree;
+            return result;
+        }
+
+        private double FindDifferenceBetweenTwoWaysOfCalculations(double firstResult, double secondResult)
+        {
+
+            return Math.Abs(firstResult - secondResult);
+        }
+
+        private int FindNearestIntegerRoot(int number, int degree)
+        {
+
+            double newtonroot = Math.Exp(Math.Log(number) / degree);
+            return (int)Math.Round(newtonroot);
+        }
+    }
+}
